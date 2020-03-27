82748c206c905bc5945f53388c6cdbdd0b1e176b
Sabyasachi Ghosh <ssgosh@gmail.com>
2020-03-27 12:35:16 +0530

    Better stats reporting. Compare Bernoulli CS vs Quant CS

--- cs.py
+++ cs.py
@@ -13,14 +13,16 @@ class CS(COMP):
 
   # Multiply actual conc matrix to M. This is the part done by mixing samples
   # and qpcr
-  def get_quantitative_results(self):
-    conc = np.expand_dims(self.conc, axis=-1)
+  def get_quantitative_results(self, conc):
+    conc = np.expand_dims(conc, axis=-1)
     #print(self.M.shape, conc.shape)
     return np.matmul(self.M.T, conc).flatten()
 
   # Initial concentration of RNA in each sample
   def create_conc_matrix_from_infection_array(self, arr):
-    conc = 1 + np.random.poisson(lam=5, size=self.n)
+    #conc = 1 + np.random.poisson(lam=5, size=self.n)
+    conc = np.random.randint(low=1, high=11, size=self.n)
+    #conc = np.ones(self.n)
     self.conc = conc * arr # Only keep those entries which are 
 
   # Solve the CS problem using Lasso
@@ -50,9 +52,36 @@ class CS(COMP):
   def decode_qp(self, results):
     pass
 
+class CSExpts:
+  def __init__(self, name):
+    self.name = name
+    self.no_error = 0
+    self.total_tp = 0
+    self.total_fp = 0
+    self.total_fn = 0
+
+  # Find results using qPCR
+  def do_single_expt(self, i, cs, x):
+    results = cs.get_quantitative_results(x)
+    score, tp, fp, fn = cs.decode_lasso(results)
+    print('%s iter = %d score: %.2f' % (self.name, i, score), 'tp = ', tp, 'fp =', fp, 'fn = ', fn)
+    if fp == 0 and fn == 0:
+      self.no_error += 1
+    self.total_tp += tp
+    self.total_fp += fp
+    self.total_fn += fn
+
+  def print_stats(self, num_expts):
+    precision = self.total_tp / float(self.total_tp + self.total_fp)
+    recall = self.total_tp / float(self.total_tp + self.total_fn)
+    print('******', self.name, 'Statistics', '******')
+    print('No errors in %d / %d cases' % (self.no_error, num_expts))
+    print('precision = %.3f, recall = %.3f' % (precision, recall))
+    print('total tp =', self.total_tp, 'total fp = ', self.total_fp, 'total fn = ', self.total_fn)
+
 def main():
   # Test width. Max number of parallel tests available.
-  t = 96*4
+  t = 384
 
   # Group size
   n = 1000
@@ -62,32 +91,21 @@ def main():
 
   # Test assignment probability. Probability that a person gets assigned to a
   # test
-  s = 0.5
+  s = 500. / 1000
 
   # lambda for regularization
   l = 0.1
 
-  no_error = 0
   num_expts = 1000
-  total_tp = 0
-  total_fp = 0
-  total_fn = 0
+  quant_csexpts = CSExpts('Quant    ')
+  ber_csexpts = CSExpts('Bernoulli')
   for i in range(num_expts):
     arr = create_infection_array_with_num_cases(n, d)
     cs = CS(n, t, s, d, l, arr)
-    results = cs.get_quantitative_results()
-    score, tp, fp, fn = cs.decode_lasso(results)
-    print('iter = %d score: %.2f' % (i, score), 'tp = ', tp, 'fp =', fp, 'fn = ', fn)
-    if fp == 0 and fn == 0:
-      no_error += 1
-    total_tp += tp
-    total_fp += fp
-    total_fn += fn
+    quant_csexpts.do_single_expt(i, cs, cs.conc)
+    ber_csexpts.do_single_expt(i, cs, arr)
+  quant_csexpts.print_stats(num_expts)
+  ber_csexpts.print_stats(num_expts)
 
-  precision = total_tp / float(total_tp + total_fp)
-  recall = total_tp / float(total_tp + total_fn)
-  print('No errors in %d / %d cases' % (no_error, num_expts))
-  print('precision = %.3f, recall = %.3f' % (precision, recall))
-  print('total fp = ', total_fp, 'total fn = ', total_fn)
 
 main()
