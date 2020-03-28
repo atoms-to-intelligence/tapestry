e4c6373a0573606c6bb70665ee4548ce186d9b20
Sabyasachi Ghosh <ssgosh@gmail.com>
2020-03-28 17:59:02 +0530

    Add some statistics

--- cs.py
+++ cs.py
@@ -4,6 +4,8 @@ from sklearn.linear_model import Lasso, LassoLars, LassoCV, LassoLarsCV
 
 from comp import create_infection_array_with_num_cases, COMP
 
+import json
+
 # Use compressed sensing to solve 0.5*||Mx - y||^2 + l * ||x||_1
 class CS(COMP):
   def __init__(self, n, t, s, d, l, arr):
@@ -30,7 +32,7 @@ class CS(COMP):
   # y is results
   def decode_lasso(self, results):
     #lasso = LassoLars(alpha=self.l)
-    lasso = Lasso(alpha=self.l, max_iter=1000)
+    lasso = Lasso(alpha=self.l, max_iter=10000)
     #lasso = LassoCV(n_alphas=100)
     lasso.fit(self.M.T, results)
     #print('best lambda = ', lasso.alpha_)
@@ -49,7 +51,20 @@ class CS(COMP):
     
     return score, tp, fp, fn
 
+  def do_cross_validation_get_lambda(self, results):
+    a = np.linspace(0.0001, 0.0010, num=10)
+    print(a)
+    ll = np.concatenate(a, 10*a, 100*a, 1000*a, 10000*a)
+    for l in ll:
+      pass
+
   def decode_qp(self, results):
+    pass
+
+  def print_matrix(self):
+    pass
+
+  def pickle_dump(self, filename):
     pass
 
 class CSExpts:
@@ -64,7 +79,7 @@ class CSExpts:
   def do_single_expt(self, i, cs, x):
     results = cs.get_quantitative_results(x)
     score, tp, fp, fn = cs.decode_lasso(results)
-    print('%s iter = %d score: %.2f' % (self.name, i, score), 'tp = ', tp, 'fp =', fp, 'fn = ', fn)
+    #print('%s iter = %d score: %.2f' % (self.name, i, score), 'tp = ', tp, 'fp =', fp, 'fn = ', fn)
     if fp == 0 and fn == 0:
       self.no_error += 1
     self.total_tp += tp
@@ -74,38 +89,76 @@ class CSExpts:
   def print_stats(self, num_expts):
     precision = self.total_tp / float(self.total_tp + self.total_fp)
     recall = self.total_tp / float(self.total_tp + self.total_fn)
-    print('******', self.name, 'Statistics', '******')
+    #print('******', self.name, 'Statistics', '******')
     print('No errors in %d / %d cases' % (self.no_error, num_expts))
-    print('precision = %.3f, recall = %.3f' % (precision, recall))
+    print('precision = %.6f, recall = %.6f' % (precision, recall))
     print('total tp =', self.total_tp, 'total fp = ', self.total_fp, 'total fn = ', self.total_fn)
+
+  def return_stats(self, num_expts):
+    precision = self.total_tp / float(self.total_tp + self.total_fp)
+    recall = self.total_tp / float(self.total_tp + self.total_fn)
+    stats = {
+        'precision'     : precision,
+        'recall'        : recall,
+        'total_tp'      : self.total_tp,
+        'total_fp'      : self.total_fp,
+        'total_fn'      : self.total_fn,
+        'no_error'      : self.no_error,
+        'expts'         : num_expts,
+        }
+
+    return stats
 
 def main():
   # Test width. Max number of parallel tests available.
-  t = 384
+  #t = 12
 
   # Group size
-  n = 1000
+  #n = 32
 
   # Number of infections. Sparsity
-  d = 40
+  #d = 1
 
   # Test assignment probability. Probability that a person gets assigned to a
   # test
   s = 500. / 1000
 
   # lambda for regularization
-  l = 0.1
+  l = 0.01
 
-  num_expts = 1000
+  num_expts = 1
   quant_csexpts = CSExpts('Quant    ')
   ber_csexpts = CSExpts('Bernoulli')
   for i in range(num_expts):
     arr = create_infection_array_with_num_cases(n, d)
     cs = CS(n, t, s, d, l, arr)
     quant_csexpts.do_single_expt(i, cs, cs.conc)
-    ber_csexpts.do_single_expt(i, cs, arr)
-  quant_csexpts.print_stats(num_expts)
-  ber_csexpts.print_stats(num_expts)
+    #ber_csexpts.do_single_expt(i, cs, arr)
+  #quant_csexpts.print_stats(num_expts)
+  return quant_csexpts.return_stats(num_expts)
+  #ber_csexpts.print_stats(num_expts)
 
 
-main()
+tt = {
+      32: range(5, 17),
+      64: range(6, 32),
+      96: range(7, 48),
+      128: range(7, 64)
+    }
+
+dd = [1, 2]
+nn = [32, 64, 96, 128]
+
+stats = {}
+for n in nn:
+  for d in dd:
+    for t in tt[n]:
+      #print('n = %d, d = %d, t = %d' % (n, d, t))
+      item = main()
+      print(item)
+      #printable = json.dumps(item, indent=4)
+      #print(printable)
+      stats['(n=%d, d=%d, t=%d)' % (n, d, t)] = item
+
+printable = json.dumps(stats, indent=4)
+print(printable)
