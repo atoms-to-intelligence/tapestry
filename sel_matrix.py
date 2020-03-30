46dd1f70c782090b4137b5e3b8ce8a6ed97e0279
ali6947 <a.rehan6947@gmail.com>
2020-03-30 10:28:09 +0530

    NNOMPCV added

--- sel_matrix.py
+++ sel_matrix.py
@@ -22,16 +22,16 @@ def find_best_matrix(n, d, t, Ms, num_expts=1000):
 
   return stats
 
-n = 60
+n = 400
 d = 2
-t = 24
+t = 64
 #Ms = [np.random.binomial(1, 0.5, size=(t, n)) for i in range(10)]
 #Ms = [optimized_M for i in range(10)]
 Ms = []
-Ms.append(optimized_M_3)
+Ms.append(optimized_M_5)
 #Ms.append(optimized_M_4)
 ss = []
-for d in range(2,7):
+for d in range(10,20):
   stats = find_best_matrix(n, d, t, Ms, num_expts=1000)
   ss.extend(stats)
   #for stat, M in zip(stats, Ms):
