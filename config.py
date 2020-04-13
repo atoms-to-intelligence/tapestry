b86f591321c024ed58b2028fffa7f79c679afea6
Sabyasachi Ghosh <ssgosh@gmail.com>
2020-04-13 19:42:53 +0530

    root_dir configuration is used by both app backend and experiments

--- config.py
+++ config.py
@@ -3,25 +3,43 @@
 # Trade off precision for recall in CS algorithms
 prefer_recall = False
 
+
+# This is the directory which contains the code. This is used to locate matrix
+# data and experimental test data in matrices.py and
+# experimental_data_manager.py. 
+#
+# The experiment code is added as a git submodule to the app backend
+# repository under folder "compute". The app backend code changes
+# config.root_dir to "compute" before importing any other file from the
+# experiments code. This needs to be "." in order to be able to execute our
+# code from the current working dir from command-line.
+#
+# Could have gone for os.environ in retrospect but this works as well.
+root_dir = '.'
+
 ########     Following configs are for the app backend only     ########
 
-root_dir = '.'
 
 # Decoding algorithm to be used with the app backend.
 app_algo = 'COMP'
+
 
 # Cycle time cutoff. Samples with greater than or equal to this value are
 # considered negative
 cycle_time_cutoff = 35
 
+
 # This is the probability of replication of RNA. Number of molecules after t
 # cycles is y_0*(1+p)**t
 p = 0.95
 
+
 ########     Following config is for synthetic experiments only     ########
+
 
 # Flip +ve bits of y with this prob
 bit_flip_prob = 0.
+
 
 # Exponential Gaussian or Variable Gaussian
 #noise_model = 'exponential_gaussian'
