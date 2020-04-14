08d44379fb5d8632b28f8cdb292186fd7f7fff74
Sabyasachi Ghosh <ssgosh@gmail.com>
2020-04-14 20:10:38 +0530

    Use 100 as default cycle time in Harvard data; Generate fake data on running experimental_data_manager.py

--- experimental_data_manager.py
+++ experimental_data_manager.py
@@ -16,7 +16,9 @@ def read_harvard_data_cts():
   fl = df.values[:, 1:]
 
   assert t == fl.shape[1]
-  cts = np.zeros(t) + config.cycle_time_cutoff
+  # config.cycle_time_cutoff may keep changing. Let's just make this 100.
+  #cts = np.zeros(t) + config.cycle_time_cutoff
+  cts = np.zeros(t) + 100
   for j in range(t):
     for i in range(fl.shape[0]):
       if fl[i, j] >= 1000:
@@ -60,4 +62,14 @@ def get_random_fake_test_data(mat_size, mat_label):
   return [i + 1 for i in pos_idx], cts
   
 if __name__ == '__main__':
-  print(read_harvard_data_cts())
+  pos_idx, cts = read_harvard_data_cts()
+  print('Harvard data (24x60)')
+  print('Positive indicies:', pos_idx)
+  print('Cycle times:', "[", ", ".join(["%.1f" % item for item in cts]), "]")
+
+  print('\nFake Data (16x40)\n')
+  for i in range(10):
+    pos_idx, cts = get_random_fake_test_data('16x40', 'optimized_M_16_40_ncbs')
+    print('Positive indicies:', pos_idx)
+    print('Cycle times:', "[", ", ".join(["%.1f" % item for item in cts]), "]")
+
