9c128e7437f9025fde868c551bac2fe5c2dcd275
Sabyasachi Ghosh <ssgosh@gmail.com>
2020-04-19 13:44:30 +0530

    Add pdf and csvs for 24x60 optimized_M_3 codename FOX

--- generate_str_matrix.py
+++ generate_str_matrix.py
@@ -40,16 +40,18 @@ def adj_list_as_csv(M, name):
   df = pd.DataFrame(data=adj_list)
   df.to_csv(name, sep=',', index=False)
 
-def adj_list_as_csv_block_format(M, name):
+def adj_list_as_csv_block_format(M, name, block_size=15, max_wells=3):
   t = M.shape[0]
   n = M.shape[1]
   adj_list = {}
-  block_size = 15
   for j in range(n):
     sample = j + 1
     tests, = np.nonzero(M[:,j])
     wells = [idx_to_well(item, t) for item in tests]
-    assert len(wells) == 3
+    assert len(wells) <= max_wells
+    if len(wells) < max_wells:
+      wells = wells + ['']*(max_wells - len(wells))
+
     # Add gap of two rows
     wells = wells + ['']*2
     if j < block_size:
@@ -71,21 +73,33 @@ def adj_list_as_csv_block_format(M, name):
   df.to_csv(name, sep=',', index=False, quoting=1)
 
 def create_all_csvs():
-  csv_dir = os.path.join(config.root_dir, "csv")
   #mlabel = "optimized_M_16_40_ncbs"
   for msize in MSizeToLabelDict:
     mlabel, _1, _2 = MSizeToLabelDict[msize]
-    mcodename = mat_codenames[mlabel]
-    mfilename = mlabel + ".csv" 
-    mfilename = os.path.join(csv_dir, mfilename)
-    adj_list_as_csv(MLabelToMatrixDict[mlabel], mfilename)
-    with open(mfilename, "a") as f:
-      f.write(f'{msize},{mcodename}\n')
+    create_csv(mlabel)
 
-csv_dir = os.path.join(config.root_dir, "csv")
-mlabel = "optimized_M_93_961_STS_1"
-mcodename = mat_codenames[mlabel]
-mfilename = mlabel + "_block.csv" 
-mfilename = os.path.join(csv_dir, mfilename)
-adj_list_as_csv_block_format(MLabelToMatrixDict[mlabel], mfilename)
+
+def create_csv(mlabel, msize, block=False):
+  csv_dir = os.path.join(config.root_dir, "csv")
+  mcodename = mat_codenames[mlabel]
+  if not block:
+    mfilename = mlabel + ".csv" 
+  else:
+    mfilename = mlabel + "_block.csv" 
+  mfilename = os.path.join(csv_dir, mfilename)
+  if not block:
+    adj_list_as_csv(MLabelToMatrixDict[mlabel], mfilename)
+  else:
+    adj_list_as_csv_block_format(MLabelToMatrixDict[mlabel], mfilename)
+  with open(mfilename, "a") as f:
+    f.write(f'{msize},{mcodename}\n')
+
+create_csv('optimized_M_3', "24x60", block=True)
+
+#csv_dir = os.path.join(config.root_dir, "csv")
+#mlabel = "optimized_M_3"
+#mcodename = mat_codenames[mlabel]
+#mfilename = mlabel + "_block.csv" 
+#mfilename = os.path.join(csv_dir, mfilename)
+#adj_list_as_csv_block_format(MLabelToMatrixDict[mlabel], mfilename)
 
