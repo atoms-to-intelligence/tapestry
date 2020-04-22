1d35577d967a6e487a7a7d48e2aefc40ed39379e
Sabyasachi Ghosh <ssgosh@gmail.com>
2020-04-22 13:34:54 +0530

    Social golfer parsing code

--- parse_social_golfer.py
+++ parse_social_golfer.py
@@ -7,16 +7,20 @@ import os
 row_map = {}
 upper = string.ascii_uppercase
 lower = string.ascii_lowercase
-for i in range(18):
+for i in range(26):
   row_map[upper[i]] = i
 
-for j in range(18):
-  row_map[lower[j]] = j + 18
+row_map['+'] = 26
+
+#for j in range(18):
+#  row_map[lower[j]] = j + 18
 
 print(row_map)
 
-M = np.zeros((36, 99), dtype=np.int32)
-with open('36x99_social_golfer.txt') as f:
+#M = np.zeros((36, 99), dtype=np.int32)
+#M = np.zeros((18, 153), dtype=np.int32)
+M = np.zeros((27, 117), dtype=np.int32)
+with open('27x117_social_golfer.txt') as f:
   lines = f.readlines()
   col = 0
   for line in lines:
@@ -27,12 +31,13 @@ with open('36x99_social_golfer.txt') as f:
     M[rows, col] = 1
     col += 1
     
-  assert col == 99  
+  print(col)
+  assert col == 117
 
 print(np.sum(M))
 print(np.sum(M, axis=1))
 print(np.sum(M, axis=0))
 
-full_path = os.path.join(mat_dir, "optimized_M_36_99_social_golfer.txt")
+full_path = os.path.join(mat_dir, "optimized_M_27_117_social_golfer.txt")
 np.savetxt(full_path, M, fmt="%d")
 
