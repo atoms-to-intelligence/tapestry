ce7e90378cdf05413bf1cf41bb99c8a9fb6d4b11
Sabyasachi Ghosh <ssgosh@gmail.com>
2020-05-13 22:10:54 +0530

    Add more tests to pythonapp.yml

--- utils/test/test_app_utils.py
+++ utils/test/test_app_utils.py
@@ -17,16 +17,15 @@ if __name__ == '__main__':
     print('y:')
     print(y)
 
-    for algo in ['COMP', 'SBL', 'combined_COMP_NNOMP_random_cv']:
-      config.app_algo = algo
-      print("\nUsing algorithm %s \n" % config.app_algo)
-      harvard_test()
+    for algo in config.app_algos:
+      print("\nUsing algorithm %s \n" % algo)
+      harvard_test(algo)
 
-  def harvard_test():
+  def harvard_test(algo):
     print("Test on Harvard Dataset")
     pos_idx, cts = read_harvard_data_cts()
     M = optimized_M_3
-    sure_list, unsure_list, neg_list, x = get_test_results(M, cts)
+    sure_list, unsure_list, neg_list, x = get_test_results(M, cts, algo)
     print("Sure list:", sure_list)
     print("Unsure list:", unsure_list)
     print("Neg list:", neg_list)
@@ -36,82 +35,28 @@ if __name__ == '__main__':
     data_file = os.path.join(config.data_dir, 'harvard', "harvard_30_120.txt") 
     cts = read_standard_cts(data_file)
     M = optimized_M_30_120_kirkman
-    config.app_algo = 'COMP'
-    print('COMP')
-    sure_list, unsure_list, neg_list, x = get_test_results(M, cts)
-    print("Sure list:", sure_list)
-    print("Unsure list:", unsure_list)
-    print("Neg list:", neg_list)
-    print("x:", x)
+    for algo in config.app_algos:
+      print(algo)
+      sure_list, unsure_list, neg_list, x = get_test_results(M, cts, algo)
+      print("Sure list:", sure_list)
+      print("Unsure list:", unsure_list)
+      print("Neg list:", neg_list)
+      print("x:", x)
 
-    config.app_algo = 'combined_COMP_SBL'
-    print(config.app_algo)
-    sure_list, unsure_list, neg_list, x = get_test_results(M, cts)
-    print("Sure list:", sure_list)
-    print("Unsure list:", unsure_list)
-    print("Neg list:", neg_list)
-    print("x:", x)
 
   def harvard_90_1140_test():
     data_file = os.path.join(config.data_dir, 'harvard', "harvard_90_1140.txt") 
     cts = read_standard_cts(data_file)
     M = optimized_M_90_1140_kirkman
-    config.app_algo = 'COMP'
-    print('COMP')
-    sure_list, unsure_list, neg_list, x = get_test_results(M, cts)
-    print("Sure list:", sure_list)
-    print("Unsure list:", unsure_list)
-    #print("Neg list:", neg_list)
+    for algo in config.app_algos:
+      print(algo)
+      sure_list, unsure_list, neg_list, x = get_test_results(M, cts, algo)
+      print("Sure list:", sure_list)
+      print("Unsure list:", unsure_list)
+      print("x:", x)
 
-    x_list = []
-    np.set_printoptions(threshold=10000)
-    config.app_algo = 'combined_COMP_SBL'
-    print(config.app_algo)
-    sure_list, unsure_list, neg_list, x = get_test_results(M, cts)
-    print("Sure list:", sure_list)
-    print("Unsure list:", unsure_list)
-    print("sure + unsure", sorted(sure_list + unsure_list))
-    print("x:", x[x>0])
-    x_list.append(np.expand_dims(x, -1))
-
-    x_list = []
-    np.set_printoptions(threshold=10000)
-    config.app_algo = 'SBL'
-    print(config.app_algo)
-    sure_list, unsure_list, neg_list, x = get_test_results(M, cts)
-    print("Sure list:", sure_list)
-    print("Unsure list:", unsure_list)
-    print("sure + unsure", sorted(sure_list + unsure_list))
-    print("x:", x[x>0])
-    x_list.append(np.expand_dims(x, -1))
-
-    #config.app_algo = 'combined_COMP_NNOMP_random_cv'
-    #print(config.app_algo)
-    #sure_list, unsure_list, neg_list, x = get_test_results(M, cts)
-    #print("Sure list:", sure_list)
-    #print("Unsure list:", unsure_list)
-    #print("sure + unsure", np.where(x)[0] + 1)
-    #print("x:", x[x>0])
-    #x_list.append(np.expand_dims(x, -1))
-
-    #config.app_algo = 'combined_COMP_l1ls_cv'
-    #min_or_max = 'max'
-    #print(config.app_algo)
-    #sure_list, unsure_list, neg_list, x = get_test_results(M, cts)
-    #print("Sure list:", sure_list)
-    #print("Unsure list:", unsure_list)
-    #print("sure + unsure", np.where(x)[0] + 1)
-    #print("x:", x[x>0])
-    #x_list.append(np.expand_dims(x, -1))
-
-    #x_combined = np.concatenate(x_list, axis=1)
-    #print(x_combined.shape)
-    #np.savetxt("x_combined_harvard_90_1140.csv", x_combined, fmt="%.6f",
-    #    delimiter=",")
-
-
-  #run_tests()
-  #harvard_30_120_test()
+  run_tests()
+  harvard_30_120_test()
   harvard_90_1140_test()
 
 
