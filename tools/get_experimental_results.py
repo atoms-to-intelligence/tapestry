43d022def170214fcbf688bd06c4b0516cda77dc
nimay-gupta <49588368+nimay-gupta@users.noreply.github.com>
2020-05-15 16:19:37 +0530

    updated get_results

--- tools/get_experimental_results.py
+++ tools/get_experimental_results.py
@@ -2,18 +2,17 @@ import sys
 sys.path.append(".")
 
 from utils.app_utils import *
+
 if __name__ == '__main__':
-	from core.matrices import MDict
 	from utils.experimental_data_manager import read_standard_cts
-	mat_label = sys.argv[1]
-	data_file = sys.argv[2]
-	cts = read_standard_cts(data_file)
-	M = MDict[mat_label]
-	for algo in config.app_algos:
-		#config.app_algo = algo
-		print(algo)
-		sure_list, unsure_list, neg_list, x = get_test_results(M, cts, algo)
-		print("Sure list:", sure_list)
-		print("Unsure list:", unsure_list)
-		print("Neg list:", neg_list)
-		print("x:", x)
+	from core.get_test_results import get_test_results
+	import argparse
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-M', '--matrix')
+	parser.add_argument('-ct', '--cycle-times')
+	parser.add_argument('-n', default=None)
+	args = parser.parse_args()
+	cts = read_standard_cts(args.cycle_times)
+	
+	print(get_test_results(args.matrix, cts, args.n)["result_string"])
